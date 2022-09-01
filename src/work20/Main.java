4584344e64566477561ac45f2b92248c1017d545
Isakova Saikal <saikal.is2308@mail.ru>
2022-09-01 18:59:00 +0600

    delete zink

--- src/work20/Main.java
+++ src/work20/Main.java
@@ -4,10 +4,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        Elements []elements = Elements.values();
-//        for (Elements e : elements) { System.out.println(e); }
-        System.out.println(Arrays.toString(Elements.values()));
+        Elements []elements = Elements.values();
+        for (Elements e : elements) { System.out.println(e); }
+//        System.out.println(Arrays.toString(Elements.values()));
     }
-
-    }
-
+}
